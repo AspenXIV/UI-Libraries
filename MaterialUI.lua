e1081c6c57d3382402da57a1be132da63acd9e2f
SkyeX9 <68152997+SkyeX9@users.noreply.github.com>
2021-05-10 14:29:02 -1000

    Update MaterialUI.lua

--- MaterialUI.lua
+++ MaterialUI.lua
@@ -610,7 +610,14 @@ function TryAddMenu(Object, Menu, ReturnTable)
 			MenuToggle = not MenuToggle
 			TweenService:Create(MenuBuild, TweenInfo.new(0.15), {Size = MenuToggle and UDim2.fromOffset(120,Size) or UDim2.fromOffset(120,0)}):Play()
 		end)
-
+		
+		function MenuButton:SetText(Value)
+			Object.Text = Value
+		end
+		function MenuButton:GetText()
+			return Object.Text
+		end
+		
 		table.foreach(Menu, function(Option, Value)
 			if typeof(Value) == "function" then
 				local MenuOption = Objects.new("SmoothButton")
